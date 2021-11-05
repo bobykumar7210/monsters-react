260294bb7e7abc7d72222b11d9918e046a244d3f
mohit <nvnkr1999@gmail.com>
2021-11-05 19:46:41 +0530

    filtercomplter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list-component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+      super()
+      this.state={
+        monsters:[],
+        searchField:""
+      }
+
+   }
+
+   //call when compoenent renders first time 
+   componentDidMount(){
+     fetch("https://jsonplaceholder.typicode.com/users")
+     .then(response=>response.json())
+     .then(users=>{
+      
+        this.setState({monsters:users})
+    })
+    .catch(reject=>console.log("faied to load"))
+   }
+   myfunction=()=>{
+     console.log('something change')
+   }
+  value="something change"
+   render(){
+
+     const {monsters,searchField}=this.state;
+      const filterMonsters=monsters.filter((monster)=>{
+        return monster.name.toLowerCase().includes(searchField.toLowerCase())
+      })
+
+     return(<div className="App">
+        <input type="search" 
+        placeholder="enter search"
+        onChange={e=>{
+          this.setState({searchField:e.target.value},()=>{
+            console.log("search : "+this.state.searchField)
+          })
+         
+        }
+        }/>
+       
+         <CardList monsters={filterMonsters}/>
+
+        
+       
+       
+      
+      
+      </div>)
+   }
+
 }
 
 export default App;
